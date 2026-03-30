2b0c44da2aade38bf2308c1bba55188da2555b70
Harsh Parihar <pariharharsh1234@gmail.com>
2026-03-30 12:43:59 +0530

    Change catch-all route to use splat parameter

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
